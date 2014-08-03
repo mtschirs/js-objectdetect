b5075d6d550a234505b022b826c28cfd8276a7b9
Martin <m.tschirsich@gmx.de>
2014-08-03 06:34:10 +0200

    Smoother optimized

--- examples/js/smoother.js
+++ examples/js/smoother.js
@@ -3,19 +3,19 @@ Smoother = function(alpha, initPos) {
 	var positions,
 		updateTime = +new Date();
 	
-	var sp = initPos, //[initPos[0], initPos[1], initPos[2], initPos[3], initPos[4]],
+	var sp = initPos,
 		sp2 = sp,
 		sl = sp.length;
 	
 	this.interpolate = true;
 	
 	this.smooth = function(pos) {
-		positions = pos;//[pos[0], pos[1], pos[2], pos[3], pos[4]];
+		positions = pos;
 		
 		// update
 		for (var i = 0;i < sl;i++) {
-			sp[i] = alpha[i] * positions[i] + (1-alpha) * sp[i];
-			sp2[i] = alpha[i] * sp[i] + (1-alpha) * sp2[i];
+			sp[i] = alpha[i] * positions[i] + (1-alpha[i]) * sp[i];
+			sp2[i] = alpha[i] * sp[i] + (1-alpha[i]) * sp2[i];
 		}
 
 		// set time
@@ -24,13 +24,7 @@ Smoother = function(alpha, initPos) {
 		var msDiff = new Date() - updateTime;
 		var newPositions = this.predict(msDiff);
 		
-		/*pos[0] = newPositions[0];
-		pos[1] = newPositions[1];
-		pos[2] = newPositions[2];
-		pos[3] = newPositions[3];
-		pos[4] = newPositions[4];*/
-		
-		return newPositions;//pos;
+		return newPositions;
 	};
 	
 	this.predict = function(letime) {
@@ -38,15 +32,15 @@ Smoother = function(alpha, initPos) {
 		var retPos = [];
 		
 		if (this.interpolate) {
-			var step = time / 1000;
-			var stepLo = step >> 0;
-			var ratio = alpha / (1-alpha);
-			
-			var a = (step - stepLo)*ratio;
-			var b = (2 + stepLo*ratio);
-			var c = (1 + stepLo*ratio);
-			
 			for (var i = 0; i < sl; i++) {
+				var step = time / 1000;
+				var stepLo = step >> 0;
+				var ratio = alpha[i] / (1-alpha[i]);
+				
+				var a = (step - stepLo)*ratio;
+				var b = (2 + stepLo*ratio);
+				var c = (1 + stepLo*ratio);
+				
 				retPos[i] = a * (sp[i] - sp2[i]) + b * sp[i] - c * sp2[i];
 			}
 		} else {
@@ -54,7 +48,7 @@ Smoother = function(alpha, initPos) {
 			var ratio = (alpha * step) / (1 - alpha);
 			var a = 2 + ratio;
 			var b = 1 + ratio;
-			for (var i = 0;i < sl;i++) {
+			for (var i = 0; i < sl; i++) {
 				retPos[i] = a * sp[i] - b * sp2[i];
 			}
 		}
